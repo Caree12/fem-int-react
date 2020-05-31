e6879969c1348fc90c042224f4b8cd37814905ae
Carrie Wladis <carriewladis@gmail.com>
2020-05-31 13:29:58 -0400

    ssr with new server folder, server/index.js on node/express, ClientApp created and added to index.html, and document removed from react initial render

---
+++ server/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import express from 'express';
+import { renderToString } from 'react-dom/server';
+import { ServerLocation } from '@reach/router';
+import fs from 'fs';
+import App from '../src/App';
+
+const PORT = process.env.PORT || 3000;
+const html = fs.readFileSync('dist/index.html').toString();
+
+// his technique here is to create an array of html so he can insert the App into the innerText of #root, aka render all above and opening div or root, insert app, render closing div and all below 
+const parts = html.split('not rendered');
+
+const app = express();
+
+// use everything in the dist dir AS the server dist
+app.use('/dist', express.static('dist'));
+
+// middleware to run everytime a req happens
+app.use((req, res) => {
+    const reactMarkup = (
+        <ServerLocation url={req.url}>
+            <App />
+        </ServerLocation>
+    );
+
+    res.send(parts[0] + renderToString(reactMarkup) + parts[1]);
+    res.end();
+});
+
+console.log('listening on ' + PORT);
+app.listen(PORT);
